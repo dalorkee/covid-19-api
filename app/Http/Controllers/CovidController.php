322ed36900f7475aa793557d759687282f873c12
dalorkee <dalorkee@gmail.com>
2020-05-26 15:59:39 +0700

    db conn

--- app/Http/Controllers/CovidController.php
+++ app/Http/Controllers/CovidController.php
@@ -14,15 +14,21 @@ class CovidController extends Controller
 	}
 
 	public function getData() {
+		DB::connection()->enableQueryLog();
 		if (DB::connection()->getDatabaseName()) {
 			echo "conncted sucessfully to database ".DB::connection()->getDatabaseName();
 			$data = DB::table('invest_pt')->select('sat_id')->where('id', '=', 1)->get();
-			dd($data);
+
 			if (count($data)) {
 				echo $data[0]->sat_id;
 			} else {
 				echo 'no query';
 			}
+
+			$queries    = DB::getQueryLog();
+			$lastQuery = end($queries);
+
+			dd($lastQuery);
 		}
 /*
 
